4478c0f85f7dd6ed9ac7469556ebca181dc0f8a2
hannahxxo <hannahantle@gmail.com>
2021-02-23 13:15:12 -0600

    First commit without bonus

---
+++ variablesExercise.js
@@ -0,0 +1,18 @@
+// const
+
+let num = 20;
+
+num = 15;
+
+num += 7;
+
+num--;
+
+num = "This is not a number";
+
+let blank = null;
+console.log(blank);
+
+// BONUS
+// var quarter, dime, nickel, penny = [0.25 0.10, 0.05, 0.01]
+// console.log(var);
